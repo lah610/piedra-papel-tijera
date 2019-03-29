1f5056a58a9875fdf9e9412aae0a4f44a612b4d3
Emiliano Alfredo Martinez Vazquez D3 <EmilianoMtz_diplomado@macbook-11.local>
2019-03-29 17:03:43 -0600

    Initial Commit

---
+++ AutoLayoutAdvanced/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutAdvanced
+//
+//  Created by Emiliano Alfredo Martinez Vazquez D3 on 3/29/19.
+//  Copyright © 2019 Emiliano Alfredo Martinez Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
