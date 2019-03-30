6077f347a5d487221d1e46edde933002d3b73da4
Luis Arenas <u1@macbook-pro-de-luis-2.local>
2019-03-30 10:13:00 -0600

    Yo agregue un nuevo archivo col izq son los cambios

---
+++ AutoLayoutAdvanced/NewFile.swift
@@ -0,0 +1,13 @@
+//
+//  NewFile.swift
+//  AutoLayoutAdvanced
+//
+//  Created by U1 on 30/03/19.
+//  Copyright © 2019 Emiliano Alfredo Martinez Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class NewFile: UICollectionReusableView {
+        
+}
